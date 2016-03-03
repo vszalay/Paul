5d0e12a2354ada2cb68662cc38d1807e2968006b
Paul von Szalay <paul.von.szalay@noaa.gov>
2016-03-03 09:59:43 -0800

    added curly braces and comment

--- rachel.sh
+++ rachel.sh
@@ -1,7 +1,8 @@
-#Xalculate reduced stats for data friles at J=100 c/bp
+#Xalculate reduced stats for data files at J=100 c/bp
+#This is pure madness!
 for datafile in "$@"
 do
-	echo $datafile
+	echo ${datafile}
 	bash goostats -J 100 -r $datafile stats-$datafile
 done
 
